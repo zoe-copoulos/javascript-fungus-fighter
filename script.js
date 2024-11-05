786f051a1bad3c4ce0bd9144c82e99aa7d61dd85
Zoe Copoulos <zoe.copoulos@gmail.com>
2024-11-05 08:23:39 -0600

    made variables for HP and AP and named a function to attack

--- script.js
+++ script.js
@@ -3,12 +3,20 @@
 // Example:
 // let fungusHP = 100;
 
+let fungusHP = 100
+let attackPoints = 100
+
 function onReady() {
     console.log("Ready to go!")
     
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
     
+
+    // function to attack
+    function attack(chosenAttack){
+
+    }
     
     // 🧠 Remember
     // - Handle events that ->
